76cac8b35abdc1d36a5aec81374d7c3cc7159f3f
Vishnu Adda <addavishnu@gmail.com>
2018-06-24 22:52:17 -0700

    Add my attempt at visualizing the insights.

---
+++ plots.R
@@ -0,0 +1,21 @@
+
+Freethrows
+Freethrowattempts
+
+plotit <- function(data, Selectplayers=1:10) {
+  Data <- data[Selectplayers,,drop=F]
+  matplot(t(Data), type="b", pch=15:18,
+          col=c(1:4,6))
+  #legend("bottomleft", inset=0.01,
+   #      legend=Players[Selectplayers], col=c(1:4,6), 
+    #     pch=15:18, horiz=F)
+}
+
+# Free Throw Attempts per game
+plotit(Freethrowattempts/Games)
+
+# Accuracy of Free Throws ( Freethrows/Freethrowattempts )
+plotit(Freethrows/Freethrowattempts)
+
+# Player playing style ( Excludes Free Throws )
+plotit((Points - Freethrows) / FieldGoals)
